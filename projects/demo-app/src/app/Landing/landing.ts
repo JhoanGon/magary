7122d60cf84aaf89f866c103cb1e724c0d87b210
Jhoan Gonzalez <zebasg.07@gmail.com>
2025-12-06 00:02:01 -0500

    feat: Add new landing page component with styling, animations, and theme switcher.

--- projects/demo-app/src/app/Landing/landing.ts
+++ projects/demo-app/src/app/Landing/landing.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { ThemeSwitcherComponent } from '../components/theme-switcher/theme-switcher.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ThemeSwitcherComponent],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
